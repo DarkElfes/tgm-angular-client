6acbb63e8d527ea5820a52f19645d812db53cee3
DarkElf <darkelf141@gmail.com>
2026-02-07 18:09:43 +0200

    Add initial configuration files and UI components
    - Created .postcssrc.json for Tailwind CSS integration.
    - Updated angular.json to include CLI analytics.
    - Added components.json to define component paths and import aliases.
    - Updated package.json and package-lock.json to include Tailwind CSS and related dependencies.
    - Introduced pnpm-lock.yaml for package management.
    - Added TypeScript configuration for component paths.
    - Implemented UI components including badges, buttons, button groups, cards, and dialogs with respective directives and styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <div>
+      <app-header/>
+      <router-outlet/>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'angular-client';
+  title = 'angular-cleent';
 }
